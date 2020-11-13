691c406e621ad1bfa1f6b813ac82041fef232606
unknown <Jenyojohn24@gmail.com>
2020-11-13 15:13:44 +1345

    setup express with node to handle file serving

---
+++ app.js
@@ -0,0 +1,19 @@
+// Imports
+const express = require("express");
+const app = express();
+const port = 5000;
+
+// Static Files
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/img", express.static(__dirname + "public/img"));
+
+// Set View's
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+// Navigation
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
